4048bcbe6e9f9d7fb9a935f8185da4fd7119ff33
Firestorm <firestorm3d@gmail.com>
2011-11-09 03:48:31 -0800

    Initial

---
+++ src/com/gatevo/aframe/AframeActivity.java
@@ -0,0 +1,13 @@
+package com.gatevo.aframe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AframeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
